4936bb46a3462822da94572823aceedbf7660bca
000hen <hen20090325@gmail.com>
2023-01-20 15:27:34 +0800

    Add for service worker

--- src/index.js
+++ src/index.js
@@ -14,7 +14,10 @@
 import ReactDOM from "react-dom/client";
 import App from "./App";
 
+import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
+
 import "./index.css";
 import "./imports.css";
 
 ReactDOM.createRoot(document.querySelector("#app")).render(<App />);
+serviceWorkerRegistration.register();
