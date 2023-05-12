b495d6879f72508c2c767ba867f30773376dd462
000hen <hen20090325@gmail.com>
2023-05-12 11:22:44 +0800

    feat: update copyright

--- src/Footer/index.js
+++ src/Footer/index.js
@@ -3,7 +3,7 @@ import "./index.css";
 export default () => {
     return <footer>
         <div>
-            <span>Copyright © 2022 <a href="https://hlhsinfo.ml">HLHSInfo Authors</a> and <a href="https://www.3zh-studio.com">sanZi Network</a></span>
+            <span>Copyright © 2023 <a href="https://hlhsinfo.ml">HLHSInfo Authors</a> and <a href="https://www.3zh-studio.com">sanZi Network</a></span>
             <span><a href="/privacy.html">Privacy Policy</a></span>
             <span>This is an <a href="https://github.com/TWMSSS/zarksdict">open-source project</a>.</span>
         </div>
