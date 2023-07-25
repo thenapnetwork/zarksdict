12c76052d5eac4a9ad79fbf0cdd046b4a995aebc
000hen <hen20090325@gmail.com>
2023-07-26 00:44:27 +0800

    feat: Update for hlhsinfo's domain

--- src/Header/index.js
+++ src/Header/index.js
@@ -8,7 +8,7 @@ export default () => {
             <img src={Logo} alt="花中查詢" width={50} />
             <div>
                 <h1>Zark 每日單字查詢器</h1>
-                <h5>Powered by <a href="https://hlhsinfo.ml">花中查詢</a></h5>
+                <h5>Powered by <a href="https://hlhsinfo.muisnowdevs.one">花中查詢</a></h5>
             </div>
         </div>
     </header>;
