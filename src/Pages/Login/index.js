46512c7e72afb3ab5647a98dea790e713575cf20
000hen <hen20090325@gmail.com>
2023-07-28 14:28:58 +0800

    feat: Fix missing argument

--- src/Pages/Login/index.js
+++ src/Pages/Login/index.js
@@ -51,10 +51,10 @@ export default () => {
                         <h3>當前不需要使用Google即可查看單字表</h3>
                         {/* <div className="center topMargin">
                             <LoginButton onClick={() => OAuthClient().requestAccessToken()} />
-                        </div>
+                        </div> */}
 
                         <Separate>或</Separate>
-                        <Button Icon={FaRegFile} onClick={() => gn("1EBd10umKJhHUaZqKtuEV8YFOsJoAGX0ffPuOO-qIAN4")}>開啟暑輔複習</Button> */}
+                        <Button Icon={FaRegFile} onClick={() => gn("1EBd10umKJhHUaZqKtuEV8YFOsJoAGX0ffPuOO-qIAN4")}>開啟暑輔複習</Button>
                     </>
                     : <h3>您無法於離線狀態下查詢線上表單</h3>
             }
